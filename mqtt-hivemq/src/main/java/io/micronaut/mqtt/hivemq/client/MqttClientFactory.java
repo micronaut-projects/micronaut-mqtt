a3366a01457d135cf30b0041681d671dd7c41527
skobow <sk@skobow.net>
2023-06-23 18:49:43 +0200

    Setup hivemq module

--- mqtt-hivemq/src/main/java/io/micronaut/mqtt/hivemq/client/MqttClientFactory.java
+++ mqtt-hivemq/src/main/java/io/micronaut/mqtt/hivemq/client/MqttClientFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.micronaut.mqtt.client;
+package io.micronaut.mqtt.hivemq.client;
 
 import io.micronaut.context.annotation.Factory;
 

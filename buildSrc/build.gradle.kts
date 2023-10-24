ca15e63e231c3a1a63f234748a01d02cec92ce58
Dean Wette <wetted@unityfoundation.io>
2023-10-24 05:03:13 -0500

    Fix java 21 (#339)
    * Update common files
    
    * fax build for Java 21
    
    ---------
    
    Co-authored-by: micronaut-build <micronaut-build-account@grails.org>
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,8 @@ repositories {
 
 dependencies {
     implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.9.28")
-    implementation("io.micronaut.gradle:micronaut-gradle-plugin:4.1.2")
-    implementation("io.micronaut.gradle:micronaut-test-resources-plugin:4.1.2")
+    implementation (libs.gradle.kotlin)
+    implementation (libs.gradle.kotlin.allopen)
+    implementation (libs.gradle.kotlin.noarg)
+    implementation (libs.micronaut.gradle.plugin)
 }

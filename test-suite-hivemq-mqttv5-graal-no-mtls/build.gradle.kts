01d2daf2751c0fed5041a60d2e8ffbd7c0d8a7f4
Jeremy Grelle <grellej@unityfoundation.io>
2024-04-03 16:07:28 -0400

    Add system props to native image build

--- test-suite-hivemq-mqttv5-graal-no-mtls/build.gradle.kts
+++ test-suite-hivemq-mqttv5-graal-no-mtls/build.gradle.kts
@@ -12,3 +12,13 @@ tasks.test {
     systemProperty("javax.net.ssl.trustStorePassword", "testtest")
     systemProperty("javax.net.ssl.trustStoreType", "jks")
 }
+
+graalvmNative {
+    binaries {
+        named("test") {
+            buildArgs.addAll("-Djavax.net.ssl.trustStore=$projectDir/mosquitto/certs/ca-cert.jks",
+                    "-Djavax.net.ssl.trustStorePassword=testtest",
+                    "-Djavax.net.ssl.trustStoreType=jks")
+        }
+    }
+}

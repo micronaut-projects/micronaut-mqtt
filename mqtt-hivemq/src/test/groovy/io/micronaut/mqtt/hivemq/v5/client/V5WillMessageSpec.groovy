dc26cc5c072fe4ab1bf1f785b5d05a5467d78326
VytautasCapgemini <vytautas.zaleckas@capgemini.com>
2024-10-01 18:09:58 +0200

    Add a second context to verify will message delivery
    This change introduces an additional application context to subscribe to the will message topic, ensuring the message is published when the first application disconnects. Removed unnecessary properties in the test and included initiating another context for proper validation.

--- mqtt-hivemq/src/test/groovy/io/micronaut/mqtt/hivemq/v5/client/V5WillMessageSpec.groovy
+++ mqtt-hivemq/src/test/groovy/io/micronaut/mqtt/hivemq/v5/client/V5WillMessageSpec.groovy
@@ -1,6 +1,5 @@
 package io.micronaut.mqtt.hivemq.v5.client
 
-
 import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient
 import com.hivemq.client.mqtt.mqtt5.message.disconnect.Mqtt5DisconnectReasonCode
 import io.micronaut.context.ApplicationContext
@@ -15,21 +14,28 @@ import spock.util.concurrent.PollingConditions
 class V5WillMessageSpec extends AbstractMQTTTest implements MQTT5Test {
 
     void "test will message"() {
+        // we create first application context that is going to disconnect.
         ApplicationContext ctx = startContext([
                 "mqtt.client.will-message.payload":"last will",
                 "mqtt.client.will-message.retained":true,
                 "mqtt.client.will-message.topic":"will/topic",
-                "mqtt.client.automatically-reconnect":true,
-                "willmessagetest": true])
+                ])
         def client = ctx.getBean(Mqtt5AsyncClient)
-        def subscriber = ctx.getBean(LastWillSubscriber)
         def polling = new PollingConditions(timeout: 3)
+
+        // second application is going to subscribe to the will message topic
+        // and track it to see if it gets published when first application disconnects
+        ApplicationContext ctx2 = startContext([
+                "willmessagetest": true])
+        def subscriber = ctx2.getBean(LastWillSubscriber)
+
 
         when:
         client.disconnectWith()
                 .reasonCode(Mqtt5DisconnectReasonCode.DISCONNECT_WITH_WILL_MESSAGE) // send the will message
                 .sessionExpiryInterval(0)                                           // we want to clear the session
-                .send();
+                .send()
+
 
         then:
         polling.eventually {
@@ -38,6 +44,7 @@ class V5WillMessageSpec extends AbstractMQTTTest implements MQTT5Test {
 
         cleanup:
         ctx.close()
+        ctx2.close()
     }
 
     @Requires(property = "willmessagetest", value = StringUtils.TRUE)
